834044a74df913b81047607ad035ccbb1c9791b0
MatiasRueda <matiasrued@gmail.com>
2024-02-28 16:52:52 -0300

    Agrego iconos

--- src/auxiliar/buscarCiudad.ts
+++ src/auxiliar/buscarCiudad.ts
@@ -1,25 +1,35 @@
 import { CiudadClima, Coordenada } from "../types/types";
-import { coordenadas, london } from "./ejemplo";
 
-const coordenada = [
+const chacarita: Coordenada[] = [
   { name: "Chacarita", lat: -34.5880107, lon: -58.4541562, country: "AR" },
 ];
 
 function cumpleCondi(arg: { cod?: number } | Array<any>): boolean {
-  return (
+  return !(
     (Array.isArray(arg) && !arg.length) ||
     (arg as { cod: number }).cod?.toString()[0] === "4"
   );
 }
 
+async function buscarCoordenadas(ciudad?: string): Promise<Coordenada[]> {
+  if (!ciudad) return chacarita;
+  const respuesta = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&appid=${process.env.EXPO_PUBLIC_CLIMA_API_KEY}`
+  );
+  const coordenadas: Coordenada[] = await respuesta.json();
+  if (!cumpleCondi(coordenadas)) return [];
+  return coordenadas;
+}
+
 export default async function buscarCiudad(
   ciudad?: string
 ): Promise<boolean | CiudadClima> {
-  const coorRespuesta: Coordenada = ciudad ? coordenadas[0] : coordenada[0]; /// FETCH : "chacarita" FETCH
-  const coorError = cumpleCondi(coorRespuesta);
-  if (coorError) return false;
-  const ciudadRespuesta: CiudadClima = london; /// FETCH
-  const ciudadError = cumpleCondi(ciudadRespuesta);
-  if (ciudadError) return false;
+  const coordenada: Coordenada[] = await buscarCoordenadas(ciudad);
+  if (!coordenada) return false;
+  const respuesta = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordenada[0].lat}&lon=${coordenada[0].lon}&lang=es&appid=${process.env.EXPO_PUBLIC_CLIMA_API_KEY}`
+  );
+  const ciudadRespuesta: CiudadClima = await respuesta.json();
+  if (!cumpleCondi(ciudadRespuesta)) return false;
   return ciudadRespuesta;
 }
